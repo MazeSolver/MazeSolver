89c95ae344475ec00fa1004e821af6af1bc8dd10
skatrak <theSkatrak@gmail.com>
2014-10-21 16:46:26 +0100

    Comienzo de la implementación de los laberintos
    Se ha implementado:
    
    - Las posibles direcciones de movimiento
    - La representación y manipulación de una celda
    - La representación de un laberinto
    - La interfaz de algoritmos para la creación de laberintos

---
+++ src/maze/Maze.java
@@ -0,0 +1,89 @@
+/**
+ * @file Maze.java
+ * @date 21/10/2014
+ */
+package maze;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+/**
+ * Clase que representa un laberinto.
+ */
+public class Maze {
+  private ArrayList <ArrayList <MazeCell>> m_maze;
+
+  /**
+   * @param alg Algoritmo de creación de laberintos ya inicializado
+   */
+  public Maze (MazeCreationAlgorithm alg) {
+    m_maze = alg.createMaze();
+  }
+
+  /**
+   * @param fileName Nombre del fichero del que cargar el laberinto.
+   * @throws IOException
+   *           Cuando no se encuentra el fichero, no se puede abrir para su
+   *           lectura o no contiene un laberinto válido.
+   */
+  public Maze (String fileName) throws IOException {
+    loadFile(fileName);
+  }
+
+  /**
+   * @param row Fila.
+   * @param column Columna.
+   * @return Celda en la posición indicada.
+   */
+  public MazeCell get (int row, int column) {
+    return m_maze.get(row).get(column);
+  }
+
+  /**
+   * @param row Fila.
+   * @param column Columna.
+   * @param cell Celda que se quiere introducir.
+   */
+  public void set (int row, int column, MazeCell cell) {
+    m_maze.get(row).set(column, cell);
+  }
+
+  /**
+   * @param fileName Nombre del fichero del que cargar el laberinto.
+   * @throws IOException
+   *           Cuando no se encuentra el fichero, no se puede abrir para su
+   *           lectura o no contiene un laberinto válido.
+   */
+  @SuppressWarnings ("unchecked")
+  public void loadFile (String fileName) throws IOException {
+    try {
+      FileInputStream fileIn = new FileInputStream(fileName);
+      ObjectInputStream in = new ObjectInputStream(fileIn);
+      m_maze = (ArrayList <ArrayList <MazeCell>>) in.readObject();
+      in.close();
+      fileIn.close();
+    }
+    catch (ClassNotFoundException c) {
+      throw new IOException(c);
+    }
+  }
+
+  /**
+   * @param fileName Nombre del fichero donde guardar el laberinto.
+   * @throws IOException
+   *           Cuando el fichero no se puede abrir o no se tienen permisos de
+   *           escritura en el mismo.
+   */
+  public void saveFile (String fileName) throws IOException {
+    FileOutputStream fileOut = new FileOutputStream(fileName);
+    ObjectOutputStream out = new ObjectOutputStream(fileOut);
+    out.writeObject(m_maze);
+    out.close();
+    fileOut.close();
+  }
+
+}
---
+++ src/maze/MazeCell.java
@@ -0,0 +1,55 @@
+/**
+ * @file MazeCell.java
+ * @date 21/10/2014
+ */
+package maze;
+
+/**
+ * Clase que representa una celda del laberinto.
+ */
+public class MazeCell {
+  private short m_cell;
+
+  /**
+   * Constructor por defecto. La celda creada no tiene muros.
+   */
+  public MazeCell () {
+    m_cell = 0;
+  }
+
+  /**
+   * Cambia el estado de la dirección especificada. Si no había muro, ahora lo
+   * hay y viceversa.
+   * @param dir Lado de la celda que se quiere modificar.
+   */
+  public void toggleWall (Direction dir) {
+    if (hasWall(dir))
+      unsetWall(dir);
+    else
+      setWall(dir);
+  }
+
+  /**
+   * Pone un muro si no lo hay en la dirección especificada.
+   * @param dir Lado de la celda que se quiere modificar.
+   */
+  public void setWall (Direction dir) {
+    m_cell |= dir.val;
+  }
+
+  /**
+   * Quita el muro si lo hay en la dirección especificada.
+   * @param dir Lado de la celda que se quiere modificar.
+   */
+  public void unsetWall (Direction dir) {
+    m_cell &= ~dir.val;
+  }
+
+  /**
+   * @param dir Lado de la celda que se quiere consultar.
+   * @return Si hay una celda en esa dirección o no.
+   */
+  public boolean hasWall (Direction dir) {
+    return (m_cell & dir.val) != 0;
+  }
+}
