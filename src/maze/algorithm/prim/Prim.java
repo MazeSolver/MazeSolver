d222c2d42808bd0a3182dc2b492b44322efb2667
Kevin Robayna <imkevinrobayna@gmail.com>
2014-10-26 22:38:55 +0000

    añadido enum auxiliar para las direcciones, lo necesitaba ya que queria numeros desde el 0 al 3, además implementado algorítmo prim para generar el laberinto, eliminado cellPrimHelper no era útil, daba más problemas de lo que ayudaba

--- src/maze/algorithm/prim/Prim.java
+++ src/maze/algorithm/prim/Prim.java
@@ -6,6 +6,7 @@ package maze.algorithm.prim;
 
 import java.util.ArrayList;
 
+import maze.Direction;
 import maze.MazeCell;
 import maze.MazeCreationAlgorithm;
 
@@ -14,7 +15,7 @@ import maze.MazeCreationAlgorithm;
  */
 public class Prim extends MazeCreationAlgorithm {
 
-  private ArrayList <ArrayList <cellPrimHelper>> mazeHelperGenerator;
+  private ArrayList <ArrayList <Boolean>> visited;
   private ArrayList <ArrayList <MazeCell>> m_maze;
 
   /**
@@ -24,10 +25,10 @@ public class Prim extends MazeCreationAlgorithm {
    */
   public Prim (int rows, int columns) {
     super(rows, columns);
-    this.mazeHelperGenerator = new ArrayList <ArrayList <cellPrimHelper>>(rows);
-    this.m_maze = new ArrayList <ArrayList<MazeCell>>(rows);
-    for (int i = 0; i < this.mazeHelperGenerator.size(); i++) {
-      this.mazeHelperGenerator.add(new ArrayList <cellPrimHelper>(columns));
+    this.visited = new ArrayList <ArrayList <Boolean>>(rows);
+    this.m_maze = new ArrayList <ArrayList <MazeCell>>(rows);
+    for (int i = 0; i < this.visited.size(); i++) {
+      this.visited.add(new ArrayList <Boolean>(columns));
       this.m_maze.add(new ArrayList <MazeCell>(columns));
     }
   }
@@ -39,8 +40,64 @@ public class Prim extends MazeCreationAlgorithm {
    */
   @Override
   public ArrayList <ArrayList <MazeCell>> createMaze () {
-    // TODO Auto-generated method stub
+    short i = 0;
+    short j = 0;
+    short nCellVisited = 1;
+    DirectionPrimHelper dummyDirection = null;
+    Direction dir = null;
+    visited.get(i).set(j, true);
+    while (nCellVisited < (this.m_columns - 1) * (this.m_rows - 1) - 1) {
+      dummyDirection = getNeighbour(i, j);
+      if (dummyDirection == dummyDirection.UP && !visited.get(i - 1).get(j)) {
+        visited.get(i - 1).set(j, true);
+        nCellVisited++;
+        m_maze.get(i).get(j).setWall(dir.UP);
+        m_maze.get(i - 1).get(j).setWall(dir.DOWN);
+        i--;
+      }
+      else if (dummyDirection == dummyDirection.LEFT && !visited.get(i).get(j - 1)) {
+        visited.get(i).set(j - 1, true);
+        nCellVisited++;
+        m_maze.get(i).get(j).setWall(dir.LEFT);
+        m_maze.get(i).get(j - 1).setWall(dir.RIGHT);
+        j--;
+      }
+      else if (dummyDirection == dummyDirection.DOWN && !visited.get(i + 1).get(j)) {
+        visited.get(i + 1).set(j, true);
+        nCellVisited++;
+        m_maze.get(i).get(j).setWall(dir.DOWN);
+        m_maze.get(i + 1).get(j).setWall(dir.UP);
+        i++;
+      }
+      else if (dummyDirection == dummyDirection.RIGHT && !visited.get(i).get(j + 1)) {
+        visited.get(i).set(j + 1, true);
+        nCellVisited++;
+        m_maze.get(i).get(j).setWall(dir.RIGHT);
+        m_maze.get(i).get(j + 1).setWall(dir.LEFT);
+        j++;
+      }
+    }
     return this.m_maze;
+  }
+
+  /**
+   *
+   * @param i
+   * @param j
+   * @return
+   *      Un vecino que sea válido (evita cruzar los bordes)
+   */
+  private DirectionPrimHelper getNeighbour (short i, short j) {
+    short MAX_NEIGHBOUR = 4;
+    short random = 0;
+    DirectionPrimHelper neighbour = null;
+    do {
+      random = (short) (0 + (Math.random() * MAX_NEIGHBOUR));
+    }
+    while ((i == 0 && random == neighbour.UP.val) || (j == 0 && random == neighbour.LEFT.val)
+        || (i == (this.m_rows - 1) && random == neighbour.DOWN.val)
+        || (j == (this.m_columns - 1) && random == neighbour.RIGHT.val));
+    return neighbour;
   }
 
 }
