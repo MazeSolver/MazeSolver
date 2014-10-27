7552fd13208c327402ba0ad7ff0e80c53e58e392
Kevin Robayna <imkevinrobayna@gmail.com>
2014-10-27 10:15:27 +0000

    Solucionado error en prim. Añadido clases auxiliares para facilitar la tarea, además se ha separado las partes del método prim para que sea mas fácil de ver el prim.

---
+++ src/maze/algorithm/prim/CellPrimHelper.java
@@ -0,0 +1,19 @@
+/**
+ * @file CellPrimHelper.java
+ * @date 27 Oct 2014
+ */
+package maze.algorithm.prim;
+
+/**
+ *
+ */
+public class CellPrimHelper {
+  public Boolean UP;
+  public Boolean LEFT;
+  public Boolean RIGHT;
+  public Boolean DOWN;
+  public Boolean visited;
+
+  public CellPrimHelper (){}
+}
+
--- src/maze/algorithm/prim/Prim.java
+++ src/maze/algorithm/prim/Prim.java
@@ -15,8 +15,9 @@ import maze.MazeCreationAlgorithm;
  */
 public class Prim extends MazeCreationAlgorithm {
 
-  private ArrayList <ArrayList <Boolean>> visited;
+  private ArrayList <ArrayList <CellPrimHelper>> IncludedCells;
   private ArrayList <ArrayList <MazeCell>> m_maze;
+  private short nCellVisited = 1;
 
   /**
    *
@@ -25,11 +26,25 @@ public class Prim extends MazeCreationAlgorithm {
    */
   public Prim (int rows, int columns) {
     super(rows, columns);
-    this.visited = new ArrayList <ArrayList <Boolean>>(rows);
+    this.IncludedCells = new ArrayList <ArrayList <CellPrimHelper>>(rows);
     this.m_maze = new ArrayList <ArrayList <MazeCell>>(rows);
-    for (int i = 0; i < this.visited.size(); i++) {
-      this.visited.add(new ArrayList <Boolean>(columns));
+    for (int i = 0; i < this.IncludedCells.size(); i++) {
+      this.IncludedCells.add(new ArrayList <CellPrimHelper>(columns));
       this.m_maze.add(new ArrayList <MazeCell>(columns));
+      for (int j = 0; j < IncludedCells.get(i).size(); j++) {
+        if (i == 0) {
+          IncludedCells.get(i).get(j).UP = true;
+        }
+        if (j == 0) {
+          IncludedCells.get(i).get(j).LEFT = true;
+        }
+        if (i == (this.m_rows - 1)) {
+          IncludedCells.get(i).get(j).DOWN = true;
+        }
+        if (j == (this.m_columns - 1)) {
+          IncludedCells.get(i).get(j).RIGHT = true;
+        }
+      }
     }
   }
 
@@ -42,63 +57,118 @@ public class Prim extends MazeCreationAlgorithm {
   public ArrayList <ArrayList <MazeCell>> createMaze () {
     short i = 0;
     short j = 0;
-    short nCellVisited = 1;
-    DirectionPrimHelper dummyDirection = null;
-    Direction dir = null;
-    visited.get(i).set(j, true);
+    Neighbour auxNeighbout = null;
+    //Empezar el laberinto con todo lleno de paredes y selecionar una celda.
+    IncludedCells.get(i).get(j).visited = true;
+    //Mientras haya celdas sin visitar, seguir visitando.
     while (nCellVisited < (this.m_columns - 1) * (this.m_rows - 1) - 1) {
-      dummyDirection = getNeighbour(i, j);
-      if (dummyDirection == dummyDirection.UP && !visited.get(i - 1).get(j)) {
-        visited.get(i - 1).set(j, true);
-        nCellVisited++;
-        m_maze.get(i).get(j).setWall(dir.UP);
-        m_maze.get(i - 1).get(j).setWall(dir.DOWN);
-        i--;
-      }
-      else if (dummyDirection == dummyDirection.LEFT && !visited.get(i).get(j - 1)) {
-        visited.get(i).set(j - 1, true);
-        nCellVisited++;
-        m_maze.get(i).get(j).setWall(dir.LEFT);
-        m_maze.get(i).get(j - 1).setWall(dir.RIGHT);
-        j--;
-      }
-      else if (dummyDirection == dummyDirection.DOWN && !visited.get(i + 1).get(j)) {
-        visited.get(i + 1).set(j, true);
-        nCellVisited++;
-        m_maze.get(i).get(j).setWall(dir.DOWN);
-        m_maze.get(i + 1).get(j).setWall(dir.UP);
-        i++;
-      }
-      else if (dummyDirection == dummyDirection.RIGHT && !visited.get(i).get(j + 1)) {
-        visited.get(i).set(j + 1, true);
-        nCellVisited++;
-        m_maze.get(i).get(j).setWall(dir.RIGHT);
-        m_maze.get(i).get(j + 1).setWall(dir.LEFT);
-        j++;
-      }
+      //Seleccionar un vecino valido para explorar
+      auxNeighbout = getNeighbour();
+      //Tirar el muro en una direccion y en la direccion de vuelta.
+      throwWall(auxNeighbout.i, auxNeighbout.j, auxNeighbout.dir);
     }
     return this.m_maze;
   }
 
   /**
    *
-   * @param i
-   * @param j
-   * @return
-   *      Un vecino que sea válido (evita cruzar los bordes)
+   * @return Un vecino que sea válido (evita cruzar los bordes)
    */
-  private DirectionPrimHelper getNeighbour (short i, short j) {
+  private Neighbour getNeighbour () {
     short MAX_NEIGHBOUR = 4;
     short random = 0;
-    DirectionPrimHelper neighbour = null;
-    do {
-      random = (short) (0 + (Math.random() * MAX_NEIGHBOUR));
+    Neighbour neighbour = null;
+    Boolean next = false;
+    short i = 0;
+    short j = 0;
+    while (i < IncludedCells.size() && !next) {
+      while (j < IncludedCells.get(i).size() && !next && IncludedCells.get(i).get(j).visited) {
+        do {
+          random = (short) (0 + (Math.random() * MAX_NEIGHBOUR));
+        }
+        while ((random == neighbour.dir.UP.val && !IncludedCells.get(i).get(j).UP)
+            || (random == neighbour.dir.LEFT.val && !IncludedCells.get(i).get(j).LEFT)
+            || (random == neighbour.dir.DOWN.val && !IncludedCells.get(i).get(j).DOWN)
+            || (random == neighbour.dir.RIGHT.val && !IncludedCells.get(i).get(j).RIGHT));
+        next = true;
+        DirectionPrimHelper aux = null;
+        aux.val = random;
+        neighbour = new Neighbour(i, j, aux);
+      }
+      j++;
+      if (j == IncludedCells.size()) {
+        i++;
+        j = 0;
+      }
+
     }
-    while ((i == 0 && random == neighbour.UP.val) || (j == 0 && random == neighbour.LEFT.val)
-        || (i == (this.m_rows - 1) && random == neighbour.DOWN.val)
-        || (j == (this.m_columns - 1) && random == neighbour.RIGHT.val));
-    neighbour.val = random;
     return neighbour;
+  }
+
+  /**
+   *
+   * @param i
+   * @param j
+   * @param auxNeighbout
+   *
+   * Abre el muro en una direccion en una posicion determinada, ademas ayuda al
+   * getNeighbour a hacer su trabajo marcando los muros como tirados.
+   *
+   */
+  private void throwWall (final short i, final short j, final DirectionPrimHelper auxNeighbour) {
+    Direction auxDirection = null;
+    if (auxNeighbour == auxNeighbour.UP) {
+      //Marcando celda destino como visitada
+      IncludedCells.get(i - 1).get(j).visited = true;
+      nCellVisited++;
+      //Tirando el muro hacia celda destino
+      m_maze.get(i).get(j).setWall(auxDirection.UP);
+      //Marcando muro hacia celda destino como tirado
+      IncludedCells.get(i).get(j).UP = true;
+      //Tirando el muro desde celda destino
+      m_maze.get(i - 1).get(j).setWall(auxDirection.DOWN);
+      //Marcando muro desde celda destino como tirado
+      IncludedCells.get(i - 1).get(j).DOWN = true;
+    }
+    else if (auxNeighbour == auxNeighbour.LEFT) {
+      //Marcando celda destino como visitada
+      IncludedCells.get(i).get(j - 1).visited = true;
+      nCellVisited++;
+      //Tirando el muro hacia celda destino
+      m_maze.get(i).get(j).setWall(auxDirection.LEFT);
+      //Marcando muro hacia celda destino como tirado
+      IncludedCells.get(i).get(j).LEFT = true;
+      //Tirando el muro desde celda destino
+      m_maze.get(i).get(j - 1).setWall(auxDirection.RIGHT);
+      //Marcando muro desde celda destino como tirado
+      IncludedCells.get(i).get(j - 1).UP = true;
+    }
+    else if (auxNeighbour == auxNeighbour.DOWN) {
+      //Marcando celda destino como visitada
+      IncludedCells.get(i + 1).get(j).visited = true;
+      nCellVisited++;
+      //Tirando el muro hacia celda destino
+      m_maze.get(i).get(j).setWall(auxDirection.DOWN);
+      //Marcando muro hacia celda destino como tirado
+      IncludedCells.get(i).get(j).DOWN = true;
+      //Tirando el muro desde celda destino
+      m_maze.get(i + 1).get(j).setWall(auxDirection.UP);
+      //Marcando muro desde celda destino como tirado
+      IncludedCells.get(i + 1).get(j).UP = true;
+    }
+    else if (auxNeighbour == auxNeighbour.RIGHT) {
+      //Marcando celda destino como visitada
+      IncludedCells.get(i).get(j + 1).visited = true;
+      nCellVisited++;
+      //Tirando el muro hacia celda destino
+      m_maze.get(i).get(j).setWall(auxDirection.RIGHT);
+      //Marcando muro hacia celda destino como tirado
+      IncludedCells.get(i).get(j).RIGHT = true;
+      //Tirando el muro desde celda destino
+      m_maze.get(i).get(j + 1).setWall(auxDirection.LEFT);
+      //Marcando muro desde celda destino como tirado
+      IncludedCells.get(i).get(j + 1).UP = true;
+    }
   }
 
 }
