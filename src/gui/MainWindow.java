2f7650fc052add804c36945d4c1a6c54dab0d834
skatrak <theSkatrak@gmail.com>
2014-11-03 19:16:08 +0000

    Creando esquema del controlador

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -4,11 +4,14 @@
  */
 package gui;
 
+import gui.environment.Environment;
 import gui.environment.EnvironmentSet;
 import gui.environment.SimpleEnvironment;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -20,10 +23,12 @@ import javax.swing.JPanel;
 import javax.swing.JSeparator;
 import javax.swing.JSlider;
 import javax.swing.JToolBar;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import maze.Maze;
 import maze.algorithm.Prim;
-import agent.SARulesAgent;
+import agent.Agent;
 
 /**
  * Ventana principal del programa. Sólo puede haber una, así que implementa el
@@ -76,7 +81,8 @@ public class MainWindow extends JFrame {
   private JButton m_run, m_step, m_pause, m_stop;
   private JSlider m_zoom;
   private JMenu m_menu_file, m_menu_maze, m_menu_agent, m_menu_help;
-  private JMenuItem m_itm_maze_open, m_itm_exit;
+  private JMenuItem m_itm_maze_new, m_itm_maze_save, m_itm_maze_open,
+                    m_itm_exit;
   private JMenuItem m_itm_maze_clone, m_itm_maze_change, m_itm_maze_close;
   private JMenuItem m_itm_agent_add, m_itm_agent_config, m_itm_agent_remove;
   private JMenuItem m_itm_about;
@@ -88,7 +94,6 @@ public class MainWindow extends JFrame {
    * para permitir el uso del programa.
    */
   private MainWindow () {
-    super();
     createInterface();
   }
 
@@ -104,15 +109,6 @@ public class MainWindow extends JFrame {
     m_global_panel = new JPanel(new BorderLayout());
     m_global_panel.add(m_toolbar, BorderLayout.NORTH);
     m_environments = new EnvironmentSet();
-
-    // XXX Sólo de prueba. Borrar cuando se haya probado la visualización del
-    // laberinto y la generación del mismo.
-    m_environments.addEnvironment(new SimpleEnvironment(new Maze(new Prim(20, 20))));
-
-    // XXX Pruebas
-    SARulesAgent ag = new SARulesAgent(new Maze(new Prim(20, 20)));
-    m_config_panel = ag.getConfigurationPanel();
-    m_global_panel.add(m_config_panel, BorderLayout.WEST);
 
     m_global_panel.add(m_environments, BorderLayout.CENTER);
     add(m_menu_bar, BorderLayout.NORTH);
@@ -133,9 +129,14 @@ public class MainWindow extends JFrame {
     m_menu_help = new JMenu("Help");
 
     // Menú "File"
+    m_itm_maze_new = new JMenuItem("New maze...");
+    m_itm_maze_save = new JMenuItem("Save maze...");
     m_itm_maze_open = new JMenuItem("Open maze...");
     m_itm_exit = new JMenuItem("Exit");
 
+    m_menu_file.add(m_itm_maze_new);
+    m_menu_file.addSeparator();
+    m_menu_file.add(m_itm_maze_save);
     m_menu_file.add(m_itm_maze_open);
     m_menu_file.addSeparator();
     m_menu_file.add(m_itm_exit);
@@ -166,6 +167,8 @@ public class MainWindow extends JFrame {
     m_menu_bar.add(m_menu_maze);
     m_menu_bar.add(m_menu_agent);
     m_menu_bar.add(m_menu_help);
+
+    setupMenuListeners();
   }
 
   /**
@@ -188,6 +191,174 @@ public class MainWindow extends JFrame {
     m_toolbar.add(new JSeparator(JSeparator.VERTICAL));
     m_toolbar.add(new JLabel("Zoom:"));
     m_toolbar.add(m_zoom);
+
+    setupToolbarListeners();
+  }
+
+  /**
+   * Crea los ActionListeners para los menús.
+   */
+  private void setupMenuListeners () {
+    // Menú "File"
+    ///////////////////////////////////////////////////////////////////////////
+    m_itm_maze_new.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed (ActionEvent e) {
+        // TODO Mostrar interfaz para elegir las dimensiones y algoritmo para
+        // generar el laberinto
+
+        // XXX Sólo para pruebas
+        m_environments.addEnvironment(new SimpleEnvironment(new Maze(new Prim(30, 30))));
+      }
+    });
+
+    m_itm_maze_save.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed (ActionEvent e) {
+        // TODO LLamar al método para mostrar la interfaz para guardar el
+        // laberinto y luego guardarlo
+      }
+    });
+
+    m_itm_maze_open.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed (ActionEvent e) {
+        // TODO Llamar al método para cargar un fichero de laberinto
+      }
+    });
+
+    m_itm_exit.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed (ActionEvent e) {
+        System.exit(0);
+      }
+    });
+
+    // Menú "Maze"
+    ///////////////////////////////////////////////////////////////////////////
+    m_itm_maze_clone.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed (ActionEvent e) {
+        Environment env = m_environments.getSelectedEnvironment();
+        if (env != null)
+          m_environments.addEnvironment(new SimpleEnvironment(env.getMaze()));
+        else {
+          // TODO Reportar error
+        }
+      }
+    });
+
+    m_itm_maze_change.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed (ActionEvent e) {
+        // TODO Llamar al método para cargar un fichero de laberinto
+        // También hay que añadir los agentes al nuevo laberinto
+      }
+    });
+
+    m_itm_maze_close.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed (ActionEvent e) {
+        m_environments.removeSelectedEnvironment();
+        repaint();
+      }
+    });
+
+    // Menú "Agent"
+    ///////////////////////////////////////////////////////////////////////////
+    m_itm_agent_add.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed (ActionEvent e) {
+        // TODO Mostrar la interfaz para la selección de un tipo de agente y
+        // agregarlo al entorno en alguna posición disponible
+      }
+    });
+
+    m_itm_agent_config.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed (ActionEvent e) {
+        try {
+          Agent ag = m_environments.getSelectedEnvironment().getSelectedAgent();
+          m_config_panel = ag.getConfigurationPanel();
+          m_global_panel.add(m_config_panel, BorderLayout.WEST);
+        }
+        catch (Exception exc) {
+          // TODO Mostrar error (No se ha podido acceder al agente seleccionado)
+          // No hay agente seleccionado o no hay entorno seleccionado
+        }
+      }
+    });
+
+    m_itm_agent_remove.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed (ActionEvent e) {
+        try {
+          Environment env = m_environments.getSelectedEnvironment();
+          m_environments.removeAgentFromEnvironment(env.getSelectedAgent(), env);
+        }
+        catch (Exception exc) {
+          // TODO Mostrar error (No se ha podido acceder al agente seleccionado)
+          // No hay agente seleccionado o no hay entorno seleccionado
+        }
+      }
+    });
+
+    // Menú "Help"
+    ///////////////////////////////////////////////////////////////////////////
+    m_itm_about.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed (ActionEvent e) {
+        // TODO Mostrar interfaz con: Hecho por X, Y, Z. Sistemas Inteligentes
+        // 2014, etc
+      }
+    });
+  }
+
+  private void setupToolbarListeners () {
+    m_run.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed (ActionEvent e) {
+        // TODO Lanzar un hilo / timer / algo que regularmente ejecute un paso
+        // de simulación en todos los entornos. Si la simulación está pausada,
+        // se reanuda.
+      }
+    });
+
+    m_step.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed (ActionEvent e) {
+        for (Environment env: m_environments.getEnvironmentList())
+          env.runStep();
+      }
+    });
+
+    m_pause.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed (ActionEvent e) {
+        // TODO Pausar el hilo / timer / algo que se esté ejecutando si hay
+        // alguno. No hacer nada si no hay nada ejecutándose o está pausado.
+      }
+    });
+
+    m_stop.addActionListener(new ActionListener() {
+      @Override
+      public void actionPerformed (ActionEvent e) {
+        // TODO Parar y eliminar el hilo / timer / algo y borrar la memoria de
+        // los agentes
+        for (Environment env: m_environments.getEnvironmentList()) {
+          for (Agent ag: env.getAgents())
+            ag.resetMemory();
+        }
+      }
+    });
+
+    m_zoom.addChangeListener(new ChangeListener() {
+      @Override
+      public void stateChanged (ChangeEvent e) {
+        JSlider src = (JSlider) e.getSource();
+        m_environments.setZoom(src.getValue() / 50.0);
+      }
+    });
   }
 
   /**
