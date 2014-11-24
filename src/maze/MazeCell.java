6bd1909d6350cba61c9474e8c00ee43685d14b7c
skatrak <theSkatrak@gmail.com>
2014-11-24 21:28:58 +0000

    Fixes #27

--- src/maze/MazeCell.java
+++ src/maze/MazeCell.java
@@ -1,3 +1,24 @@
+/*
+ * This file is part of MazeSolver.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright (c) 2014 MazeSolver
+ * Sergio M. Afonso Fumero <theSkatrak@gmail.com>
+ * Kevin I. Robayna Hernández <kevinirobaynahdez@gmail.com>
+ */
+
 /**
  * @file MazeCell.java
  * @date 21/10/2014
